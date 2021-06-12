dbbb3b1d3c39d38a720d1605904123f7c4989124
Gal Weizman <weizmangal@gmail.com>
2021-06-12 23:03:12 +0300

    support usage of custom object constructor

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const isWindow = require('is-window');
 const {DST_IS_NOT_A_WINDOW, SRC_IS_NOT_A_WINDOW, SRC_IS_NOT_SAME_ORIGIN_AS_WINDOW} = require('./errors');
 
+function isWindow(object) {
+  const o = object(obj);
+  return o === o.window;
+}
+
 function isCrossOrigin(dst, src) {
     return src.Object.getPrototypeOf(dst) === null;
 }
 
-module.exports = function(dst, src) {
+module.exports = function(dst, src, object = Object) {
     src = src || window;
-    if (!isWindow(src)) {
+    if (!isWindow(src, object)) {
         throw new Error(SRC_IS_NOT_A_WINDOW);
     }
-    if (!isWindow(dst)) {
+    if (!isWindow(dst, object)) {
         throw new Error(DST_IS_NOT_A_WINDOW);
     }
     if (isCrossOrigin(window, src)) {
