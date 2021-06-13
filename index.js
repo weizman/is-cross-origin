d066b974282d027fa22b2d9b5d071bd1b05cf56e
Gal Weizman <weizmangal@gmail.com>
2021-06-13 23:51:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const isWindow = require('is-window');
 const {DST_IS_NOT_A_WINDOW, SRC_IS_NOT_A_WINDOW, SRC_IS_NOT_SAME_ORIGIN_AS_WINDOW} = require('./errors');
 
-function isWindow(object) {
-  const o = object(obj);
-  return o === o.window;
+function isWindow(obj, Object) {
+    const o = Object(obj);
+    return o === o.window;
 }
 
-function isCrossOrigin(dst, src) {
-    return src.Object.getPrototypeOf(dst) === null;
+function isCrossOrigin(dst, src, Object) {
+    return Object.getPrototypeOf.call(src, dst) === null;
 }
 
-module.exports = function(dst, src, object = Object) {
-    src = src || window;
-    if (!isWindow(src, object)) {
+module.exports = function(dst, src = window, Object = window.Object) {
+    if (!isWindow(src, Object)) {
         throw new Error(SRC_IS_NOT_A_WINDOW);
     }
-    if (!isWindow(dst, object)) {
+    if (!isWindow(dst, Object)) {
         throw new Error(DST_IS_NOT_A_WINDOW);
     }
-    if (isCrossOrigin(window, src)) {
+    if (isCrossOrigin(window, src, Object)) {
         throw new Error(SRC_IS_NOT_SAME_ORIGIN_AS_WINDOW);
     }
-    return isCrossOrigin(dst, src);
+    return isCrossOrigin(dst, src, Object);
 };
